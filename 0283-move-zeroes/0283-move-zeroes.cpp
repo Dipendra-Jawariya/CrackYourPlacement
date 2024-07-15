adde92af8202567d4be8d1946f8b98f421890477
Dipendra Jawariya <75981473+Dipendra-Jawariya@users.noreply.github.com>
2024-07-15 09:52:12 +0530

    Time: 11 ms (90.57%), Space: 21.9 MB (10.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // in place
         int n = nums.size();
-        vector<int> ans;
+        int zero_ind = -1;
+        
         for(int i = 0; i < n; i++) {
-            if(nums[i] != 0) {
-                ans.push_back(nums[i]);
+            if(nums[i] == 0) {
+                zero_ind = i;
+                break;
             }
         }
-        nums.clear();
-        nums = ans;
-        for(int i = nums.size() ; i < n; i++) {
-            nums.push_back(0);
+        if(zero_ind == -1) return;
+        
+        for(int j = zero_ind + 1; j < n; j++) {
+            if(nums[j] != 0) {
+                swap(nums[j] , nums[zero_ind]);
+                zero_ind++;
+            }
         }
     }
 };
